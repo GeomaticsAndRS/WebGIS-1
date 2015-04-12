8c0b2c5a81c81953af0ba3e31ed6a96743c96c60
Alexander <alexander.i.kamenev@mail.com>
2015-04-13 00:25:50 +0300

    make admin

--- map/admin.py
+++ map/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Attribute, Feature, Layer, AttributeValue
 
 # Register your models here.
+admin.site.register(Attribute)
+admin.site.register(Feature)
+admin.site.register(Layer)
+admin.site.register(AttributeValue)
