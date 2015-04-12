53cf65098e34d37aa8b35deba4bb83da43efa2ad
Alexander <alexander.i.kamenev@mail.com>
2015-04-13 00:10:45 +0300

    create model

--- map/models.py
+++ map/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Layer(models.Model):
+    layername = models.CharField(max_length=255)
+    srs_wkt = models.CharField(max_length=255)
+    geom_type = models.CharField(max_length=50)
+    encoding = models.CharField(max_length=20)
+
+
+class Attribute(models.Model):
+    layer = models.ForeignKey(Layer)
+    name = models.CharField(max_length=255)
+    type = models.IntegerField()
+    width = models.IntegerField()
+    precision = models.IntegerField()
+
+
+class Feature(models.Model):
+    layer = models.ForeignKey(Layer)
+    geom_point = models.PointField(srid=4326, blank=True, null=True)
+    geom_multipoint = models.MultiPointField(srid=4326, blank=True, null=True)
+    geom_multilineString = models.MultiLineStringField(srid=4326, blank=True, null=True)
+    geom_multipolygon = models.MultiPolygonField(srid=4326, blank=True, null=True)
+    geom_geometrycollection = models.GeometryCollectionField(srid=4326, blank=True, null=True)
+
+    objects = models.GeoManager()
+
+
+class AttributeValue(models.Model):
+    feature = models.ForeignKey(Feature)
+    attribute = models.ForeignKey(Attribute)
+    value = models.CharField(max_length=255, blank=True, null=True)
