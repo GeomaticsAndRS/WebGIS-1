41a99af6bef8249ba8d921b7022cbe4ea2ba422f
__freeman__ <alexander.i.kamenev@gmail.com>
2015-05-06 17:54:12 +0300

    static geojson

--- map/views.py
+++ map/views.py
@@ -3,6 +3,7 @@ from .models import Points
 from django.core.serializers import serialize
 from django.http import HttpResponse
 
+
 def map_view(request):
     return render_to_response('index.html')
 
