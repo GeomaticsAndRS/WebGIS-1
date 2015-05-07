787470a7493235da4a11a77028bdc34d6e2869ad
__freeman__ <alexander.i.kamenev@gmail.com>
2015-05-07 10:02:32 +0300

    main_page manipulations

--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+from django.template import Context
 
 
 def main_page(request):
-    return render_to_response('main_page.html')
+    c = Context({'user_name': request.user.username})
+    return render_to_response('main_page.html', context=c)
